b79ac8584fe8ce15e9d7009ba56237a6eca167dc
baijiayong <jiayongbai@gmail.com>
2014-03-11 23:22:04 +0800

    helloworld1

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,8 @@ public class HelloWorld{
   {
     System.out.println("Hello World"); 
     System.out.println("你好世界");
+    System.out.println("kongniqiwa");
+    System.out.println("hello World!");
+    System.out.println("hello World!!");
   }
 }
