7b5e3ae649002bfeec840983a4affe50e769171b
baijiayong <jiayongbai@gmail.com>
2014-03-12 12:38:15 +0800

    增加了good morning

--- hibaldur.java
+++ hibaldur.java
@@ -3,5 +3,7 @@ public class Hibaldur
   public static void main(String args[])
   {
     System.out.println("hi baldur");
+    System.out.println("hi tech");
+    Systen.out.println("good morning");
   }
-}
+}                
